01664e7e5f5efc6faa3f5633dbcb8f1809ed9549
Mykhailo Trunov <mykhailo.trunov@synapse.com>
2020-09-29 00:53:14 +0300

    Updates

---
+++ precache-manifest.6978a7b516309295e17e587371d4ab44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a910c7064de02842985ea89a59f721a7",
+    "url": "/react-hw-phonebook/index.html"
+  },
+  {
+    "revision": "909c6265b06c5d58bc4c",
+    "url": "/react-hw-phonebook/static/css/main.cd542fdf.chunk.css"
+  },
+  {
+    "revision": "2679e1dc8c28a5efb2f7",
+    "url": "/react-hw-phonebook/static/js/2.69df2b92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-phonebook/static/js/2.69df2b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909c6265b06c5d58bc4c",
+    "url": "/react-hw-phonebook/static/js/main.e272420a.chunk.js"
+  },
+  {
+    "revision": "b8cce74442cf5fded36e",
+    "url": "/react-hw-phonebook/static/js/runtime-main.ea3f15be.js"
+  }
+]);
